2f5f2f3756374c8ad45c1e7ce2eeef7dc30336d4
Milos Havlicek <miloshavlicek@gmail.com>
2022-12-10 22:08:36 +0100

    Fix profile image url

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ function HomePage() {
       <ProfileCard
         name="Miloš Havlíček"
         jobTitle="Technologický a startupový nadšenec s vášní pro osobní rozvoj a networking"
-        profilePhoto="https://media-exp1.licdn.com/dms/image/C4D03AQG6Wec3xqZzaA/profile-displayphoto-shrink_800_800/0/1602862523499?e=1675900800&v=beta&t=onmFIF-rAysMr323C0_-LKuafNovR2tT-PaZIKfeakk"
+        profilePhoto="/img/profile/havlicek-left.jpg"
         description="Vývojář s chutí změnit svět prostřednictvím technologií, který si během své kariéry uvědomil, že tím nejdůležitějším, co rozhoduje o úspěchu a spokojenosti každého z nás jsou silné mezilidské vztahy a míra důvěry. Začal se tak více zaměřovat na budování vztahů, obchod a s tím související vyjednávání."
         linkToFacebook="https://www.facebook.com/miloshavlicek"
         linkToLinkedIn="https://www.linkedin.com/in/miloshavlicek/"
